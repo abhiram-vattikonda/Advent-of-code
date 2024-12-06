b5f8482f39b94f9e3c5d7b6e18de3eae0c9dc701
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-06 15:04:33 +0530

    implemented most of it

--- day-5.py
+++ day-5.py
@@ -19,3 +19,24 @@ while True:
     updates.append(y)
 
 
+correct = []
+iscorrect = True
+for i in range(0, len(updates)):
+    iscorrect = True
+    for j in range(1, len(updates[i])):
+        for k in range(0, j):
+            try:
+                if updates[i][k] in rules[updates[i][j]]:
+                    iscorrect = False
+                    break
+            except KeyError:
+                continue
+        if iscorrect is False:
+            break
+    if iscorrect is True:
+        correct.append(updates[i])
+    
+sum = 0
+for i in range(0, len(correct)):
+    print(correct[i][len(correct[i]/2 + 1)])
+print(sum)
