80990a19ebb4b6268adce4ecb98c43d542e4eb96
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-06 09:59:02 +0530

    taking inputs

--- day-5.py
+++ day-5.py
@@ -1,1 +1,24 @@
-print("Hello")
+rules = {str : list()}
+while True:
+    rawrule = input()
+    if rawrule == "":
+        break
+    x = rawrule.partition(" | ")
+    try:
+        rules[x[0]].append(x[2])
+    except KeyError:
+        rules.update({x[0] : []})
+        rules[x[0]].append(x[2])
+
+updates = []
+while True:
+    rawupdate = input()
+    if rawupdate == "":
+        break
+    y = rawupdate.split(",")
+    updates.append(y)
+
+
+print(rules)
+
+print(updates)
