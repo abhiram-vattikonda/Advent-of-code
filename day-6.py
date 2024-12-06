2d29a6134dcf1b88d8cb5b2f44f1d9b101940233
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-06 16:12:14 +0530

    part one complete

--- day-6.py
+++ day-6.py
@@ -3,13 +3,47 @@ while True:
     raw = input()
     if raw == "":
         break
-    map.append(raw)
+    map.append(list(raw))
 
 x = 0
 y = 0
 for i in range(0, len(map)):
-    x = map[i].find("^")
-    if x != -1:
+    try:
+        x = map[i].index("^")
         y = i
+    except ValueError:
+        continue
 
-print(map[y])
+char = '^'
+count = 0
+while True:
+    try:
+        map[y][x] = 'X'
+
+        if char == '^' and map[y-1][x] == "#":
+            char = '>'
+        elif char == 'v' and map[y+1][x] == "#":
+            char = '<'
+        elif char == '>' and map[y][x+1] == "#":
+            char = 'v'
+        elif char == '<' and map[y][x-1] == "#":
+            char = '^'
+        
+        if char == '^':
+            y -= 1
+        elif char == 'v':
+            y += 1
+        elif char == '>':
+            x += 1
+        elif char == '<':
+            x -= 1
+
+        if map[y][x] =='.':
+            count += 1
+            map[y][x] = char
+
+    except IndexError:
+        break
+
+
+print(count + 1)
