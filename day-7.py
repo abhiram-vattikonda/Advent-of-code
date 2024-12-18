15661e7a635dc2c8c3a5de221e53c0c92f2f7b26
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-18 08:36:34 +0530

    day 7

--- day-7.py
+++ day-7.py
@@ -1,8 +1,17 @@
-def canform(result, nums):
+
+# this is a form of branch and bound
+def canform(result, nums, i, sumtillnow, concat):
+    #the two base cases
+    if i == len(nums):
+        return result == sumtillnow
     
+    if sumtillnow > result:
+        return False
     
-    
-    pass
+    #Check if adding of multiplying the current sum and next will give the ans
+    # if concat is true, also check for the operation with concatination
+    return canform(result, nums, i+1, sumtillnow + nums[i], concat) or (concat and canform(result, nums, i+1, sumtillnow * nums[i], concat) or canform(result, nums, i+1, int(str(sumtillnow) + str(nums[i])), concat))
+
 
 def aoc7():
     #take in put
@@ -14,14 +23,19 @@ def aoc7():
         data.append(x)
     
     #parse input and find solution
-    total = 0
+    total1 = 0
+    total2 = 0
     for i in data:
         result, nums = i.split(": ")
         result = int(result)
         nums = [int(x) for x in nums.split()]
-        if canform(result, nums):
-            total += 1
-        
-    print(total)
+        if canform(result, nums, 1, nums[0], False):
+            total1 += result
+
+        if canform(result, nums, 1, nums[0], True):
+            total2 += result
+
+    print(total1)
+    print(total2)
 
 aoc7()
