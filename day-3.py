43aaf8d69a8e8cbe3dad1ee891dd7f003fffebb3
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-19 14:19:23 +0530

    day 3 part 2 done

--- day-3.py
+++ day-3.py
@@ -7,13 +7,26 @@ def aoc3():
             data.append(line)
 
     sum = 0
+    do = True
 
-    reg = r"mul\(\d{1,3},\d{1,3}\)"
+    reg = r"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))"
     for x in data:
         li = re.findall(reg, x)
+        #print(li)
         for y in li:
-            a,b = re.search(r"\d{1,3},\d{1,3}", y)[0].split(',')
-            sum += int(a) * int(b)
+            if do == False and y[1] == 'do()':
+                do =True
+                continue
+            
+            if do == True and y[2] == "don't()":
+                do = False
+                continue
+
+            if do == True and y[1] == '' and y[2] == '':
+                a,b = re.search(r"\d{1,3},\d{1,3}", y[0])[0].split(',')
+                sum += int(a) * int(b)
+
+            #print(sum)
 
     print(sum)
 aoc3()
