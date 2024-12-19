5d7978b75544e52a5322d520a7b46aa47774ade3
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-19 12:44:26 +0530

    started day 2

---
+++ day-2.py
@@ -0,0 +1,14 @@
+
+
+def aoc2():
+    data = []
+
+    with open("puzzle2.txt", 'r') as file:
+        for line in file:
+            a, b = line.split()
+            A.append(int(a))
+
+
+
+
+aoc2()
