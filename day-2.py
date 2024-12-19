46a6a45072ef77674ffba131c59051b75c415787
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-19 13:05:12 +0530

    day 2 part 1 finished

--- day-2.py
+++ day-2.py
@@ -5,10 +5,35 @@ def aoc2():
 
     with open("puzzle2.txt", 'r') as file:
         for line in file:
-            a, b = line.split()
-            A.append(int(a))
+            data.append([int(x) for x in line.split()])
 
+    points = 0
+    for x in data:
+        correct = True
+        increasing = True
+        change = False
+        for i, y in enumerate(x):
+            if i == len(x) - 1:
+                break
+        
+            if y > x[i+1] and i == 0:
+                increasing = False
 
+            if y < x[i+1] and increasing == False:
+                change = True
+            if y > x[i+1] and increasing == True:
+                change = True
+
+            diff = abs(y - x[i+1])
+            if 1 > diff or diff > 3:
+                correct = False
+                break
+        
+        if correct and not change:
+            points += 1
+
+    #print(data)
+    print(points)
 
 
 aoc2()
