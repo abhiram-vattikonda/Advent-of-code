37807856fe4372ca37c03486c17903fab21805be
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-19 12:40:36 +0530

    finished part 2 of day 1

--- day-1.py
+++ day-1.py
@@ -14,12 +14,25 @@ def aoc1():
 
 
     sum = 0
+    sim = dict({})
 
     for i, x in enumerate(A):
         #print(f"{x} and {B[i]}")
         sum += abs(x - B[i])
+        sim[x] = 0
+
+    for x in B:
+        if x in sim:
+            sim[x] += 1
+        
+
+    newsum = 0
+    for x in A:
+        #print(f"{x} and {sim[x]}")
+        newsum += x * sim[x]
     
     print(sum)
+    print(newsum)
 
 
 aoc1()
