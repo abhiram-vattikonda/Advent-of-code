e72e283ec23bd1da8a9da6d0f63e1156cae6245f
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-20 11:41:57 +0530

    ...

--- day-14.py
+++ day-14.py
@@ -1,5 +1,8 @@
 # add two new lines for the puzzle brfore doing
 
+def parsedata(data : list[str]):
+    
+    return
 
 def aoc13():
     data = []
@@ -7,23 +10,6 @@ def aoc13():
         for line in file:
             data.append(line.strip())
 
-    mechines = int(len(data) / 4)
-    print(mechines)
-
-    a, b, end = parsedata(data)
-    sum = 0
-    for i in range(mechines):
-        n = -(b[i][0]*end[i][1] - b[i][1]*end[i][0]) / (a[i][0]*b[i][1] - a[i][1]*b[i][0])
-        m = -(end[i][0]*a[i][1] - end[i][1]*a[i][0]) / (a[i][0]*b[i][1] - a[i][1]*b[i][0])
-        
-        if n - round(n) == 0:
-            n = int(n)
-        if m - round(m) == 0:
-            m = int(m)
-        
-        if type(n) == int and type(m) == int:
-            sum += 3*n + m
-      
-    print(sum)
+    
 
 aoc13()
