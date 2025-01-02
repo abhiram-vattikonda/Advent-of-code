5cea0c84b3d4ce45a93fb84f97ed0364f442c68f
abhee238 <abhiram.vattikonda@gmail.com>
2025-01-02 11:55:39 +0530

    finished day 9 part 2

--- day-9.py
+++ day-9.py
@@ -89,9 +89,78 @@ def p1():
 
     print(sum)
 
+
 def p2():
     file = open(name, 'r')
     line = file.read()
+
+    val = []
+    spaces = {}
+    positions = {}
+    positionpos = {}
+    pos = 0
+    for i, x in enumerate(line):
+        if (i % 2) == 0:
+            positionpos.update({pos : len(val)})
+            val += [pos] * int(x)
+            positions.update({pos : int(x)})
+            
+            pos += 1
+        else:
+            spaces.update({len(val) : int(x)})
+            val += [-1] * int(x)
+            
+    #print([f"{x} : {i}" for i,x in enumerate(val)])
+
+    
+    #print(positions)
+    #print(spaces)
+
+    pos -= 1
+    #print(val)
+
+    while pos >= 0:
+        t = positions.pop(pos)
+        for i in spaces:
+            if spaces[i] >= t and i < positionpos[pos]:
+                spaces[i] -= t
+                val = [-1 if q == pos else q for q in val]
+                for _ in range(t):
+                    positionpos.pop(pos)
+                    positionpos.update({pos : i})
+                    val[i + _] = pos
+        
+
+                spaces.clear()
+                loc = 0
+                while loc < len(val):
+                    if val[loc] == -1:
+                        temploc = loc
+                        nums = 0
+                        while loc < len(val) and val[loc] == -1:
+                            nums += 1
+                            loc += 1
+                        spaces.update({temploc : nums})
+                        continue
+                    loc += 1
+                break
+        
+        #print(val)
+        print(pos)
+
+        pos -= 1
+
+    print(val)
+    
+    sum = 0
+    for i, a in enumerate(val):
+        if a == -1:
+            continue
+        sum += a * i
+
+    print(sum)
+
+
 
 
 p1()
