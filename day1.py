edb0e9dd195dc55ef287718374a768d8e4023ef7
abhiram-vattikonda <abhiram.vattikonda@gmail.com>
2025-12-01 19:30:53 +0530

    finished day 1
    previously didn't acount for more than 1 full spin per rotation

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 import re
 
-with open("test.txt") as f:
+with open("inp1.txt") as f:
     rotations = f.read().split("\n")
 
 dial  = 50
@@ -15,25 +15,37 @@ zeros = 0
 #         print(f"no L or R in {rotation}")
 #     if dial == 0:
 #         zeros += 1
-print(rotations)
+
+
 for rotation in rotations:
+    initialdial = dial
     if 'L' in rotation:
-        dial = (dial - int(rotation[1:]))
+        rc = int(rotation[1:])
+        dialchange = rc % 100
+        spins = rc // 100
+        dial -= dialchange
 
     elif 'R' in rotation:
-        dial = (dial + int(rotation[1:]))
+        rc = int(rotation[1:])
+        dialchange = rc % 100
+        spins = rc // 100
+        dial += dialchange
+
 
     else:
         print(f"no L or R in {rotation}")
 
-    print(rotation, "  ", dial, end = " ")
-    if dial >= 100 or dial <= 0:
+    zeros += spins
+    if (dial >= 100 or dial <= 0) and initialdial != 0:
         zeros += 1
-        dial %= 100
-        print(dial)
-        print(" +1 = ", zeros)
+        #print("+1  ", end="")
+
+    #print(f"{rotation}  {initialdial} -->  {dial}")
+    dial %= 100
+
     # dial %= 100
-    # print(dial)
+
+#2621 is low
 
 
 
