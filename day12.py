685ee1fea57363a2a3ba44213e364562a814b0fe
abhiram-vattikonda <abhiram.vattikonda@gmail.com>
2025-12-25 21:51:23 +0530

    day 9

--- day12.py
+++ day12.py
@@ -1,24 +1,28 @@
 import time
 start_time =  time.time()
 
-with open("test.txt") as f:
+with open("inp12.txt") as f:
     lines = f.read()
 
 class shape:
     points = []
     size = (0,0)
     center = (0, 1)
+    area = 0
     
     def __init__(self, shape :list[str]):
         self.points = []
         self.size = (len(shape), len(shape[0]))
         self.center = (len(shape)//2, len(shape[0])//2)
+        area = 0
         for i, line in enumerate(shape):
             for j, syn in enumerate(line):
                 if syn == '#':
                     self.points.append((j, i))
+                    area += 1
         
         list(set(self.points))
+        self.area = area
 
     def rotate_right(self):
         cx, cy = self.center
@@ -59,9 +63,18 @@ for shape_str in shapes_str:
 spaces = [space.split(' ') for space in spaces]
 spaces = [[tuple(map(int, (space[0][:-1]).split('x'))), list(map(int, space[1:]))] for space in spaces]
 
-for shp in shapes:
-    shp.rotate_left()
-    shp.print_shape()
-    print()
+# Part 1 but using a trick, this only works for this input not a general solution
+# we should solve it using pack
+fits_count = 0
+for space, gifts in spaces:
+    area_present = space[0] * space[1]
+    area_gifts = 0
+    for i, k in enumerate(gifts):
+        area_gifts += k * shapes[i].area
+
+    if area_present > area_gifts:
+        fits_count += 1
+
+print("Part 1: ", fits_count)
 
 print(f"----- {time.time() - start_time} -----")
--- day9.py
+++ day9.py
@@ -1,22 +1,23 @@
 import time
 start_time = time.time()
 
-with open("test.txt") as f:
+with open("inp9.txt") as f:
     lines = f.read().splitlines()
 
 lines = [tuple(map(int, line.split(","))) for line in lines]
 
 # PART 1
-# m = 0
-# for i,line in enumerate(lines):
-#     for j in lines[i:]:
-#         area = abs(line[0] - j[0] + 1) * abs(line[1] - j[1] + 1)
-#         if area > m:
-#             m = area
-# print(m)
+m = 0
+for i,line in enumerate(lines):
+    for j in lines[i:]:
+        area = abs(line[0] - j[0] + 1) * abs(line[1] - j[1] + 1)
+        if area > m:
+            m = area
+print(m)
+print(f"----- {time.time() - start_time} -----")
 
 # PART 2
-def area(line1, line2):
+def area(line1 :tuple[int, int], line2 :tuple[int, int]):
     return abs(line1[0]-line2[0] + 1) * abs(line1[1]-line2[1] + 1)
 
 def add_btw_points(l1, l2):
@@ -44,93 +45,130 @@ def add_btw_points(l1, l2):
 
     return new_points
 
- 
+
+def print_grid(rows :int, columns :int, points :list[tuple]):
+    for j in range(rows):
+        for i in range(columns):
+            if (i, j) in points:
+                print("#", end='')
+            else:
+                print(".", end='')
+        print()
+
+    
+def rec_fill(inside_point :tuple[int, int], grid :list[list[int]]):
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+    
+    stack = [inside_point]
+    new_points = []
+
+    while stack:
+        p = stack.pop()
+
+        if not (0 <= p[0] < len(grid[0]) and 0 <= p[1] < len(grid)):
+            continue
+
+        if grid[p[1]][p[0]] == 1:
+            continue
+
+        new_points.append(p)
+        grid[p[1]][p[0]] = 1
+        
+        for direction in directions:
+            new_point = (p[0] + direction[0], p[1] + direction[1])
+            stack.append(new_point)
+
+    return new_points, grid
+
+allx, ally = [], []
+for a, b in lines:
+    allx.append(a)
+    ally.append(b)
+
+unqx = sorted(set(allx))
+unqy = sorted(set(ally))
+mapx = {x : i for i, x in enumerate(unqx)}
+mapy = {y : i for i, y in enumerate(unqy)}
+
+compressed_points = [(mapx[a], mapy[b]) for a,b in lines]
+
+# boundry points
 points = []
-l = len(lines)
+l = len(compressed_points)
 for i in range(l):
-   points.extend(add_btw_points(lines[i % l], lines[(i + 1) % l]))
+   points.extend(add_btw_points(compressed_points[i % l], compressed_points[(i + 1) % l]))
 points = list(set(points))
+
+
+grid = [[1 if (j, i) in points else 0 for j in range(len(unqx))] for i in range(len(unqy))]
+print(f"{len(grid)}x{len(grid[0])}")
 print("Boundry")
 
-# points = sorted(points)
-# print(points)
-def print_grid(grid):
-    for i in grid:
-        print(i)
+# ray casting
+inside = (-1, -1)
+for j in range(len(unqy)):
+    for i in range(len(unqx)):
+        if (i, j) in points:
+            continue
+
+        fence_c = 0 # no of #'s
+        for k in range(i, len(unqx)):
+            if (k, j) in points:
+
+                if ((k-1, j) not in points or (k+1, j) not in points):
+                    fence_c += 1
+                
         
-max_x = max([x for x, y in lines]) + 3
-max_y = max([y for x, y in lines]) + 3
-grid = ['.'*(max_x)]*max_y
-for i in range(max_y):
-    # print(i)
-    for j in range(max_x):
-        if (j, i) in lines:
-            grid[i] = f"{grid[i][:j]}#{grid[i][j+1:]}"
+        if fence_c and fence_c % 2 != 0:
+            inside = (i, j)
+            break
+    
+    if inside != (-1, -1):
+        break
 
-for x, y in points:
-    if (x, y) not in lines:
-        grid[y] = f"{grid[y][:x]}X{grid[y][x+1:]}"
+print("raytrace")
 
-borders = grid.copy()
+# print_grid(len(unqx), len(unqy), points)
+print(inside)
+new_points, grid = rec_fill(inside, grid)
+points.extend(new_points)
+# print_grid(len(unqx), len(unqy), points)
+print("fill")
 
+# Calc max area
+mapx = {mapx[a] : a for a in mapx}
+mapy = {mapy[a] : a for a in mapy}
+b_area = 0
+fin_points = (a, b)
 
-all_points_x = points.copy()
-for y in range(max_y):
-    white = True
-    for x in range(max_x):
-        if white and grid[y][x] == 'X':
-            white = False
-        elif not white and grid[y][x] == 'X':
-            white = True
-        
-        if not white and grid[y][x] == '.':
-            grid[y] = f"{grid[y][:x]}X{grid[y][x+1:]}"
-            all_points_x.append((x, y))
+for a in compressed_points:
+    for b in compressed_points:
+        cona = (mapx[a[0]], mapy[a[1]])
+        conb = (mapx[b[0]], mapy[b[1]])
 
+        k = area(cona, conb)
+        if k > b_area and grid[b[1]][a[0]] and grid[a[1]][b[0]]:
+            hole = False
+            for w in range(min(a[1], b[1]), max(a[1], b[1])):
+                for q in range(min(a[0], b[0]), max(a[0], b[0])):
+                    if not grid[w][q]:
+                        hole = True
+                        break
+                if hole:
+                    break
+            if hole:
+                continue
+            b_area = max(b_area, k)
+            fin_points = (cona, conb)
+            ma, mb = a, b
 
-grid = borders.copy()
-all_points_y = points.copy()
-for x in range(max_x):
-    white = True
-    for y in range(max_y):
-        if white and grid[y][x] == 'X':
-            white = False
-        elif not white and grid[y][x] == 'X':
-            white = True
-        
-        if not white and grid[y][x] == '.':
-            grid[y] = f"{grid[y][:x]}X{grid[y][x+1:]}"
-            all_points_y.append((x, y))
+print((ma, mb))
+print(fin_points)
+print("Part 2: ", area(fin_points[0], fin_points[1]))
 
-print_grid(grid)
+# 176819620 is too low
+# 1603403500 is wrong
+# 4600046948 is too high
 
-all_points = [a for a in all_points_y if a in all_points_x]
-
-grid = ['.'*(max_x)]*max_y
-for y in range(max_y):
-    for x in range(max_x):
-        if (x, y) in all_points:
-            grid[y] = f"{grid[y][:x]}X{grid[y][x+1:]}"
-
-print_grid(grid)
-
-
-# all_points = []
-# for i, p1 in enumerate(points):
-#     print(i)
-#     for j, p2 in enumerate(points[i+1:]):
-#         all_points.extend(add_btw_points(p1, p2))
-#     all_points = list(set(all_points))
-#     print("\b", time.time() - start_time) 
-
-# m = 0
-# for i, p1 in enumerate(lines):
-#     print(i)
-#     for j, p2 in enumerate(lines[i+1:]):
-#         c1 = (p1[0], p2[1])
-#         c2 = (p2[0], p1[1])
-#         if c1 in all_points and c2 in all_points:
-#             m = max(m, abs(p1[0] - p2[0] + 1) * abs(p1[1] - p2[1] + 1))   
-#     print("\b", time.time() - start_time) 
-# print(m)
 print(f"----- {time.time() - start_time} -----")
